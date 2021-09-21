6f2d6ffb3fa80dd0a8d63540ae509a79d0e8ef03
Tammy <tammyanndo@gmail.com>
2021-09-21 18:54:57 -0400

    code for spinner2.js

--- spinner2.js
+++ spinner2.js
@@ -1,1 +1,9 @@
-console.log('hello');
+let spinner = ['|', '/', '-', '\\', '|', '/', '-', '\\' ]
+let counter = 0
+
+for (const x of spinner) {
+  counter += 200
+  setTimeout(() => {
+  process.stdout.write(`\r${x}    `);
+  }, counter)
+}
